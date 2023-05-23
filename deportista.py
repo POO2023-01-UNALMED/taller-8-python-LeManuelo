fdc56fe7bd295cf6d292f402b90377910c218342
LeManuelo <lemanuelo04s@gmail.com>
2023-05-22 20:50:06 -0500

    Commit_2

--- deportista.py
+++ deportista.py
@@ -1,15 +1,13 @@
 class Deportista:
-    def __init__(self, años_practicando, deporte="Futbol"):
-        self._deporte = deporte
+    def __init__(self, años_practicando):
+        self._deporte = "Futbol"
         self._años_practicando = años_practicando
 
-    # --------- getters and setters-------------
     def getDeporte(self):
         return self._deporte
-    def setDeporte(self, deporte: str):
-        self._deporte = deporte
-
+    
     def getAñosPracticando(self):
         return self._años_practicando
+    
     def setAñosPracticando(self, años_practicando):
         self._años_practicando = años_practicando
